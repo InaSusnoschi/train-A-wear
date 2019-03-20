8f5fcb101ba84278c6366cfc2a393fddeb478171
InaSusnoschi <ioana.susnoschi@yahoo.com>
2019-03-20 23:40:00 +0100

    App v1.00
    first version of the app

--- app/src/main/java/com/example/first_app/MainActivity.java
+++ app/src/main/java/com/example/first_app/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.first_app;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.content.Intent;
+import android.widget.Button;
 import android.widget.EditText;
 
-import static android.provider.AlarmClock.EXTRA_MESSAGE;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -16,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /** Called when the user taps the Send button */
+    /** Called when the user taps the Setup button to transfer the message*/
     public void sendMessage(View view) {
         // Do something in response to button
         Intent intent = new Intent(this, DisplayMessageActivity.class);
@@ -26,6 +39,37 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    /**
+     * background process for connecting to RPI and sending get request
+     */
+
+
+
+    private class Background_get extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... params){
+            try{
+                /* Change IP to IP set in arduino sketch
+
+                 */
+                URL url = new URL("http://192.168.1.148/?" + params[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder result = new StringBuilder();
+                String inputLine;
+                while ((inputLine = in.readLine()) != null)
+                    result.append(inputLine).append("\n");
+
+                in.close();
+                connection.disconnect();
+                return result.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
     /** Called when user taps the "workout" button*/
     public void enterWorkout (View view) {
         // enter new screen in response to button
@@ -35,5 +79,6 @@ public class MainActivity extends AppCompatActivity {
         //intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(workout);
     }
+
 
 }
