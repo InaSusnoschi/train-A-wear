a754935aa185ed1c47ef75af9c074e0e1251d325
maggie_ivanova <margaritaivanova95@gmail.com>
2019-04-13 12:49:32 +0100

    Integration algorithms class and header file containing the pitch and roll calibration algorithms.

---
+++ Server/Integration_algorithms.h
@@ -0,0 +1,20 @@
+#ifndef INTEGRATION_ALGORITHMS_H
+#define INTEGRATION_ALGORITHMS_H
+
+
+class Integration_algorithms
+{
+    public:
+        complementary_filter();
+        Roll();
+        Pitch();
+        squat_straight_back();
+        squat_bend_right_knee();
+        squat_bend_right_knee();
+
+    protected:
+
+    private:
+};
+
+#endif // INTEGRATION_ALGORITHMS_H
